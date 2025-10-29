fbf69690a6b2200803c15195ba6c754eb4281b3e
nathanlng <nathan.vouak@gmail.com>
2025-10-29 16:05:15 +0100

    projects navbar

--- assets/js/carroussel.js
+++ assets/js/carroussel.js
@@ -6,6 +6,7 @@ const trackB = document.getElementById("trackB");
 const contentA = document.getElementById("contentA");
 const contentB = document.getElementById("contentB");
 const contentC = document.getElementById("contentC");
+const contentD = document.getElementById("contentD");
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -17,6 +18,7 @@ buttons.forEach(btn => {
             contentA.textContent = "Contenu A - Page 1";
             contentB.textContent = "Contenu B - Page 1";
             contentC.textContent = "Contenu C - Page 1";
+            contentD.textContent = "Contenu D - Page 1";
 
         } else if (page === "2") {
             trackA.style.transform = "translateX(-50%)";
@@ -24,7 +26,26 @@ buttons.forEach(btn => {
             contentA.textContent = "Contenu A - Page 2";
             contentB.textContent = "Contenu B - Page 2";
             contentC.textContent = "Contenu C - Page 2";
+            contentD.textContent = "Contenu D - Page 2";
         }
     });
 });
 
+const items = document.querySelectorAll('.nav-item');
+const highlight = document.querySelector('.highlight');
+
+function moveHighlight(element) {
+  highlight.style.top = element.offsetTop + 'px';
+  highlight.style.height = element.offsetHeight + 'px';
+}
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelector('.active')?.classList.remove('active');
+    item.classList.add('active');
+    moveHighlight(item);
+  });
+});
+
+// Position initiale
+moveHighlight(document.querySelector('.active'));
